5d946e5ccea8aa795f0252993d536bc191f81b9c
fallnomega <fallnomega@gmail.com>
2023-02-17 09:16:59 -0800

    UI setup

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import tkinter as tk
 
-
-
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -12,11 +10,30 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
+
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
+def count_down():
+    return
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
-window = tk.Tk
+window = tk.Tk()
+window.title("Pomodoro")
+window.minsize(width=300, height=350)
+window.config(padx=100, pady=50, bg=GREEN)
+
+# background setup
+bgimg = tk.PhotoImage(file="tomato.png")
+# bg = tk.Label(image=bgimg)
+# # bg.place(x=0,y=0)
+# bg.pack()
+canvas = tk.Canvas(width=200, height=224,bg=GREEN, highlightthickness=False)
+canvas.create_image(103, 112, image=bgimg)
+canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
+canvas.pack()
+
+window.mainloop()
